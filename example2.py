6a3de4e283669138f6fc4dcd9fea0bc0a97fa136
Ismael Zavala <ismael.zavala@oracle.com>
2022-09-22 10:20:53 -0500

    implemented path masses trace

--- example2.py
+++ example2.py
@@ -3,12 +3,20 @@ import sys
 from math import sin, cos, pi
 
 
+COLOR_WHITE = (255,255,255)
+COLOR_GREEN = (0,255,0)
+COLOR_RED = (255,0,0)
+COLOR_BLUE = (0,0,255)
+COLOR_BLACK = (0,0,0)
+ORIGIN = (0,0)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 500))
 clock = pygame.time.Clock()
 background = pygame.Surface((800,500))
+background.fill(COLOR_BLACK)
 
-COLOR_WHITE = (255,255,255)
+
 
 # line lenght
 l1 = 100
@@ -21,27 +29,34 @@ a1 = pi/4
 a2 = pi/8
 # masses position
 v0 = pygame.Vector2(400,200)
-v1 = pygame.Vector2(l1*sin(a1),l1*cos(a1)) + v0
-v2 = pygame.Vector2(l2*sin(a2),l2*cos(a2)) + v1
-# lines draw
-line1 = pygame.draw.line(background, COLOR_WHITE, v0, v1, 2)
-line2 = pygame.draw.line(background, COLOR_WHITE, v1, v2, 2)
-# masses draw
-mass1 = pygame.draw.circle(background, COLOR_WHITE, v1, m1, 0)
-mass2 = pygame.draw.circle(background, COLOR_WHITE, v2, m2, 0)
+
 
 while True:
+
 	events = pygame.event.get()
 	for event in events:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+	
+	v1 = pygame.Vector2(l1*sin(a1),l1*cos(a1)) + v0
+	v2 = pygame.Vector2(l2*sin(a2),l2*cos(a2)) + v1
+
+	# lines draw
+	pygame.draw.line(screen, COLOR_GREEN, v0, v1, 2)
+	pygame.draw.line(screen, COLOR_RED, v1, v2, 2)
+	# masses draw
+	pygame.draw.circle(screen, COLOR_GREEN, v1, m1, 0)
+	pygame.draw.circle(screen, COLOR_RED, v2, m2, 0)
+	# masses path draw
+	pygame.draw.circle(background, COLOR_GREEN, v1, 1, 0)
+	pygame.draw.circle(background, COLOR_RED, v2, 1, 0)
+	
 	a1 += 0.01
+	a2 += 0.1
 	
-	print(v1,v2)
-	v1.update(a1,a1)
-	line1.update()
-	
-	screen.blit(background, (0,0))
 	pygame.display.update()
-	clock.tick(1)
+	clock.tick(30)
+	screen.blit(background, ORIGIN)
+	
+	
